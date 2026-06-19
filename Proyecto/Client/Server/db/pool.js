2c4bf6dcbe477ada00819997ade2d13f1a545df9
Eduardo Gomez <eduman.000@gmail.com>
2026-06-19 11:34:38 -0300

    db

--- Proyecto/Client/Server/db/pool.js
+++ Proyecto/Client/Server/db/pool.js
@@ -1,12 +1,19 @@
 const { Pool } = require("pg");
 const env = require("../config/env");
 
-const pool = new Pool({
-  user: env.DB_USER,
-  host: env.DB_HOST,
-  database: env.DB_NAME,
-  password: env.DB_PASSWORD,
-  port: env.DB_PORT,
-});
+const pool = env.DATABASE_URL
+  ? new Pool({
+    connectionString: env.DATABASE_URL,
+    ssl: {
+      rejectUnauthorized: false,
+    },
+  })
+  : new Pool({
+    user: env.DB_USER,
+    host: env.DB_HOST,
+    database: env.DB_NAME,
+    password: env.DB_PASSWORD,
+    port: env.DB_PORT,
+  });
 
 module.exports = pool;
